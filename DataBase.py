40f60066488f8cf49ae070e783f738be3121df3b
Adrian Sandoval <asandoval2107@gmail.com>
2025-05-08 09:45:52 -0500

    Setup structure for the databases and setup temp placeholder database variables and functions

--- DataBase.py
+++ DataBase.py
@@ -1,2 +1,27 @@
+import sqlite3
 
+ACTIVITY_DB = 'user_activity.db'
+DEMO_DB = 'demo.db' #temp database
 
+def init_db():
+    conn = sqlite3.connect(ACTIVITY_DB)
+    cursor = conn.cursor()
+    cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS user_activity (
+        id INTEGER PRIMARY KEY,
+        user_name TEXT,
+        message TEXT,
+        timestamp TEXT)"""
+    )
+    conn.commit()
+    conn.close()
+
+def log_activity(user:str, message:str):
+    # insert new activity record
+    pass
+
+# Temp function build out based on actual database
+def query_books(filters:dict )->list:
+    # Build and execute SQL query on DEMO_DB based on filters
+    pass

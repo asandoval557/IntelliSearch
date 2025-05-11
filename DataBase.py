98b8dced739763dbcd10f414b74385fa4bcf8264
Adrian Sandoval <asandoval2107@gmail.com>
2025-05-11 07:34:17 -0500

    Added create_user DB to store registered users account information which will be used to authenticate users access when logging into the application.

--- DataBase.py
+++ DataBase.py
@@ -1,4 +1,6 @@
 import sqlite3
+import hashlib
+import secrets
 
 ACTIVITY_DB = 'user_activity.db'
 DEMO_DB = 'demo.db' #temp database
@@ -36,6 +38,24 @@ def log_activity(user:str, message:str, timestamp:str = None):
     )
     conn.commit()
     conn.close()
+
+def create_user(username: str, password: str, role: str = None) ->bool:
+    """Register new user with hashed password and random salt."""
+    salt = secrets.token_hex(16)
+    pwd_hash = hashlib.pbkdf2_hmac('sha256'. password.encode(),salt.encode(),10000).hex()
+    try:
+        conn = sqlite3.connect(ACTIVITY_DB)
+        cursor = conn.cursor()
+        cursor.execute(
+            'INSERT INTO user_auth (username, password_hash, salt, role) VALUES (?, ?, ?, ?)',
+            (username, pwd_hash, salt, role)
+        )
+        conn.commit()
+        return True
+    except sqlite3.IntegrityError:
+        return False
+    finally:
+        conn.close()
 
 # Temp function build out based on actual database
 def query_books(filters:dict )->list:
